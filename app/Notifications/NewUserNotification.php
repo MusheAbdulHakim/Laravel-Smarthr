d835b7cd743f54582525823994c36a95d5862509
musheabdulhakim <musheabdulhakim99@gmail.com>
2021-07-20 15:13:26 +0000

    notification

---
+++ app/Notifications/NewUserNotification.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class NewUserNotification extends Notification
+{
+    use Queueable;
+
+    public $user;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail','database'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        $url = route('users',['id' => $this->user['id']]);
+        return (new MailMessage)
+                    ->line('A new user has been created')
+                    ->line($this->user['name'].' created and account with username: '. $this->user['username'].' and email '. $this->user['email'])
+                    ->action('View User', $url)
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            'user_id' => $this->user['id'],
+        ];
+    }
+}
