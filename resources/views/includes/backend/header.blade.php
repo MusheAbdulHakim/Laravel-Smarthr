7cfe8745bfd519498ca0ae26d9f1676ce0defa5c
musheabdulhakim <musheabdulhakim99@gmail.com>
2021-07-20 15:13:42 +0000

    get unread notifications

--- resources/views/includes/backend/header.blade.php
+++ resources/views/includes/backend/header.blade.php
@@ -31,84 +31,35 @@
         <!-- Notifications -->
         <li class="nav-item dropdown">
             <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
-                <i class="fa fa-bell-o"></i> <span class="badge badge-pill">3</span>
+                <i class="fa fa-bell-o"></i> <span class="badge badge-pill">{{auth()->user()->notifications->count()}}</span>
             </a>
             <div class="dropdown-menu notifications">
                 <div class="topnav-dropdown-header">
                     <span class="notification-title">Notifications</span>
-                    <a href="javascript:void(0)" class="clear-noti"> Clear All </a>
+                    <a href="{{route('clear-all')}}" class="clear-noti"> Clear All </a>
                 </div>
                 <div class="noti-content">
                     <ul class="notification-list">
-                        <li class="notification-message">
-                            <a href="activities.html">
-                                <div class="media">
-                                    <span class="avatar">
-                                        <img alt="" src="assets/img/profiles/avatar-02.jpg">
-                                    </span>
-                                    <div class="media-body">
-                                        <p class="noti-details"><span class="noti-title">John Doe</span> added new task <span class="noti-title">Patient appointment booking</span></p>
-                                        <p class="noti-time"><span class="notification-time">4 mins ago</span></p>
+                        @foreach (auth()->user()->unreadNotifications as $notification)
+                            <li class="notification-message">
+                                <a href="{{route('activities')}}">
+                                    <div class="media">
+                                        <span class="avatar">
+                                            <img alt="user" src="{{asset('storage/users/'.auth()->user()->avatar)}}">
+                                        </span>
+                                        <div class="media-body">
+                                            <p class="noti-details"><span class="noti-title">{{auth()->user()->name}}</span> {{$notification->type}}
+                                                 <span class="noti-title">This is the notification body</span></p>
+                                            <p class="noti-time"><span class="notification-time">{{$notification->created_at->diffForHuman()}}</span></p>
+                                        </div>
                                     </div>
-                                </div>
-                            </a>
-                        </li>
-                        <li class="notification-message">
-                            <a href="activities.html">
-                                <div class="media">
-                                    <span class="avatar">
-                                        <img alt="" src="assets/img/profiles/avatar-03.jpg">
-                                    </span>
-                                    <div class="media-body">
-                                        <p class="noti-details"><span class="noti-title">Tarah Shropshire</span> changed the task name <span class="noti-title">Appointment booking with payment gateway</span></p>
-                                        <p class="noti-time"><span class="notification-time">6 mins ago</span></p>
-                                    </div>
-                                </div>
-                            </a>
-                        </li>
-                        <li class="notification-message">
-                            <a href="activities.html">
-                                <div class="media">
-                                    <span class="avatar">
-                                        <img alt="" src="assets/img/profiles/avatar-06.jpg">
-                                    </span>
-                                    <div class="media-body">
-                                        <p class="noti-details"><span class="noti-title">Misty Tison</span> added <span class="noti-title">Domenic Houston</span> and <span class="noti-title">Claire Mapes</span> to project <span class="noti-title">Doctor available module</span></p>
-                                        <p class="noti-time"><span class="notification-time">8 mins ago</span></p>
-                                    </div>
-                                </div>
-                            </a>
-                        </li>
-                        <li class="notification-message">
-                            <a href="activities.html">
-                                <div class="media">
-                                    <span class="avatar">
-                                        <img alt="" src="assets/img/profiles/avatar-17.jpg">
-                                    </span>
-                                    <div class="media-body">
-                                        <p class="noti-details"><span class="noti-title">Rolland Webber</span> completed task <span class="noti-title">Patient and Doctor video conferencing</span></p>
-                                        <p class="noti-time"><span class="notification-time">12 mins ago</span></p>
-                                    </div>
-                                </div>
-                            </a>
-                        </li>
-                        <li class="notification-message">
-                            <a href="activities.html">
-                                <div class="media">
-                                    <span class="avatar">
-                                        <img alt="" src="assets/img/profiles/avatar-13.jpg">
-                                    </span>
-                                    <div class="media-body">
-                                        <p class="noti-details"><span class="noti-title">Bernardo Galaviz</span> added new task <span class="noti-title">Private chat module</span></p>
-                                        <p class="noti-time"><span class="notification-time">2 days ago</span></p>
-                                    </div>
-                                </div>
-                            </a>
-                        </li>
+                                </a>
+                            </li>
+                        @endforeach
                     </ul>
                 </div>
                 <div class="topnav-dropdown-footer">
-                    <a href="activities.html">View all Notifications</a>
+                    <a href="{{route('activities')}}">View all Notifications</a>
                 </div>
             </div>
         </li>
