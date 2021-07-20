6db73ae03497f6cee15d87c3c8b0234e3d3aac78
musheabdulhakim <musheabdulhakim99@gmail.com>
2021-07-20 12:07:03 +0000

    added username

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,6 +24,7 @@ class UserFactory extends Factory
     {
         return [
             'name' => $this->faker->name(),
+            'username' => Str::random(6),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
