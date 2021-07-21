3cca8691f20729bd86e5ba2c204fe69d5d4a372c
musheabdulhakim <musheabdulhakim99@gmail.com>
2021-07-21 19:14:17 +0000

    update user

--- app/Http/Controllers/Backend/UserController.php
+++ app/Http/Controllers/Backend/UserController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Backend;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use App\Notifications\NewUserNotification;
 
 class UserController extends Controller
 {
@@ -30,7 +32,27 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request,[
+            'name' => 'required|max:100',
+            'username' => 'required|max:10',
+            'email' => 'required|email',
+            'password' => 'required|confirmed|max:200|min:5',
+            'avatar'=>'nullable|file|image|mimes:jpg,jpeg,png,gif',
+        ]);
+        $imageName = null;
+        if($request->hasFile('avatar')){
+            $imageName = time().'.'.$request->avatar->extension();
+            $request->avatar->move(public_path('storage/users'), $imageName);
+        }
+        $user = User::create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'avatar' => $imageName,
+        ]);
+        $user->notify(new NewUserNotification($user));
+        return back()->with('success',"New user has been added");
     }
 
     /**
@@ -48,12 +70,37 @@ class UserController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * 
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request)
     {
-        //
+        $this->validate($request,[
+            'name' => 'required|max:100',
+            'username' => 'required|max:10',
+            'email' => 'required|email',
+            'password' => 'nullable|confirmed|max:200|min:5',
+            'avatar'=>'nullable|file|image|mimes:jpg,jpeg,png,gif',
+        ]);
+        $user = User::findOrFail($request->id);
+        $imageName = $user->avatar;
+        if($request->hasFile('avatar')){
+            $imageName = time().'.'.$request->avatar->extension();
+            $request->avatar->move(public_path('storage/users'), $imageName);
+        }
+        $password = $user->password;
+        if($request->password){
+           $password= Hash::make($request->password);
+        }
+        $user->update([
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => $password,
+            'avatar' => $imageName,
+        ]);
+        return back()->with('success',"User has been updated!!!");
+
     }
 
     /**
