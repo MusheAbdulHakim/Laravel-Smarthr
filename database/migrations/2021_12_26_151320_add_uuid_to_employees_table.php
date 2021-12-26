ffb735ac70398235e91c371569a31db363154ce4
musheabdulhakim <musheabdulhakim99@gmail.com>
2021-12-26 15:25:34 +0000

    employee uuid added

---
+++ database/migrations/2021_12_26_151320_add_uuid_to_employees_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUuidToEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('employees', function (Blueprint $table) {
+            $table->string('uuid')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('employees', function (Blueprint $table) {
+            $table->dropColumn('uuid');
+        });
+    }
+}
