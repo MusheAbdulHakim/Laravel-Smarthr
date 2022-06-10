7ec16d3b327644f4c304d68088c0b0e569268326
Musheabdulhakim <musheabdulhakim99@gmail.com>
2022-06-10 03:40:20 +0000

    updated

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -8,23 +8,23 @@
         <title>{{ucfirst(config('app.name'))}} - {{ucfirst($title)}}</title>
 		<!-- Favicon -->
         <link rel="shortcut icon" type="image/x-icon" href="{{!empty(app(App\Settings\ThemeSettings::class)->favicon) ? asset('storage/settings/'.app(App\Settings\ThemeSettings::class)->favicon):asset('assets/img/favicon.png')}}">
-		
 		<!-- Bootstrap CSS -->
         <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
-		
 		<!-- Fontawesome CSS -->
         <link rel="stylesheet" href="{{asset('assets/css/font-awesome.min.css')}}">
+		<!-- Lineawesome CSS -->
+        <link rel="stylesheet" href="{{asset('assets/css/line-awesome.min.css')}}">
+		<!-- Datetimepicker CSS -->
+		<link rel="stylesheet" href="{{asset('assets/css/bootstrap-datetimepicker.min.css')}}">
+		
+		<!-- Toastr Css -->
+		<link rel="stylesheet" href="{{asset('assets/plugins/toastr/toastr.min.css')}}">
+		<!-- Toastify css -->
+		<link rel="stylesheet" href="{{asset('assets/plugins/toastify/src/toastify.css')}}">
 		
 		<!-- Main CSS -->
         <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
-		
-		<!-- Lineawesome CSS -->
-        <link rel="stylesheet" href="{{asset('assets/css/line-awesome.min.css')}}">
-	
-		<!-- Datetimepicker CSS -->
-		<link rel="stylesheet" href="{{asset('assets/css/bootstrap-datetimepicker.min.css')}}">
-
-
+		<!-- Page Css -->
 		@yield('styles')
 
 		<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
@@ -90,32 +90,56 @@
 		
     </body>
 	<!-- jQuery -->
-	<script src="{{asset('assets/js/jquery-3.2.1.min.js')}}"></script>
-		
+	<script src="{{asset('assets/plugins/jquery/jquery.min.js')}}"></script>
 	<!-- Bootstrap Core JS -->
 	<script src="{{asset('assets/js/popper.min.js')}}"></script>
 	<script src="{{asset('assets/js/bootstrap.min.js')}}"></script>
-	
 	<!-- Slimscroll JS -->
 	<script src="{{asset('assets/js/jquery.slimscroll.min.js')}}"></script>
-	
-	<!-- Custom JS -->
-	<script src="{{asset('assets/js/app.js')}}"></script>
-
 	<!-- Datetimepicker JS -->
 	<script src="{{asset('assets/js/moment.min.js')}}"></script>
 	<script src="{{asset('assets/js/bootstrap-datetimepicker.min.js')}}"></script>
-
 	<!-- Ck Editor -->
 	<script src="{{asset('assets/plugins/ckeditor/ckeditor.js')}}"></script>
-	
+	<!-- Toastr JS -->
+	<script src="{{asset('assets/plugins/toastr/toastr.min.js')}}"></script>
+	<!-- Toastify JS -->
+	<script src="{{asset('assets/plugins/toastify/src/toastify.js')}}"></script>
+	<!-- Custom JS -->
+	<script src="{{asset('assets/js/app.js')}}"></script>
 	<script>
 		$(document).ready(function (){
-			$('.deletebtn').on('click',function (){
+			$('body').on('click','.deletebtn',function (){
 				$('#delete_modal').modal('show');
 				var id = $(this).data('id');
 				$('#delete_id').val(id);
-			})
+			});
+			$('.alert').delay(2000).fadeOut();
+            @if(Session::has('message'))
+                var type = "{{ Session::get('alert-type', '') }}";
+                switch (type) {
+                    case 'info':
+                        toastr.info("{{ Session::get('message') }}");
+                        break;
+                    
+                    case 'success':
+                        toastr.success("{{ Session::get('message') }}");
+                        break;
+                    
+                    case 'warning':
+                        toastr.warning("{{ Session::get('message') }}");
+                        break;
+                    
+                    case 'error':
+                        toastr.error("{{ Session::get('message') }}");
+                        break;
+                    
+                    case 'danger':
+                        toastr.error("{{ Session::get('message') }}");
+                        break;
+                    
+                }
+            @endif
 		});
 	</script>
 	@yield('scripts')
