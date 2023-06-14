b5d30a37e7709cf46204e69b7ee501d10e405bb6
musheabdulhakim <musheabdulhakim99@gmail.com>
2023-06-14 01:38:27 -0700

    jobs factory

---
+++ database/factories/JobFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Department;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class JobFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence(),
+            'department_id' => Department::factory(),
+            'location' => $this->faker->address(),
+            'vacancies' => $this->faker->numberBetween(1,10),
+            'experience' => $this->faker->numberBetween(1,5),
+            'age' => $this->faker->numberBetween(19,50),
+            'salary_from' => $this->faker->numberBetween(100,1000),
+            'salary_to'=> $this->faker->numberBetween(101,1001),
+            'type' => $this->faker->randomElement(['Full Time','Part Time','Internship','Temporary','Remote','Others']),
+            'status' => $this->faker->randomElement(['Open','Cancelled','Closed']),
+            'start_date' => $this->faker->date(),
+            'expire_date' => $this->faker->date(),
+            'description' => $this->faker->realTextBetween(),
+        ];
+    }
+}
