2a5f62768d96ea3e61cd12e92cdd3e62bb43d16b
musheabdulhakim <musheabdulhakim99@gmail.com>
2024-06-24 14:05:16 +0000

    init

---
+++ database/factories/DepartmentFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Department>
+ */
+class DepartmentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->word(),
+            'location' => $this->faker->streetAddress(),
+            'description' => $this->faker->realText()
+        ];
+    }
+}
---
+++ database/migrations/2024_06_24_102055_create_holidays_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('holidays', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->date('startDate');
+            $table->date('endDate');
+            $table->longText('description')->nullable();
+            $table->boolean('is_annual')->nullable()->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('holidays');
+    }
+};
