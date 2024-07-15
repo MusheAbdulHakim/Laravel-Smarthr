fcf268f93a702a5b8a0d8480e892fb05fad18677
musheabdulhakim <musheabdulhakim99@gmail.com>
2024-07-15 08:37:21 +0000

    active class updated

--- Modules/Roles/app/Listeners/AppMenuListener.php
+++ Modules/Roles/app/Listeners/AppMenuListener.php
@@ -23,6 +23,6 @@ class AppMenuListener
     {
         $menu = $event->menu;
         $menu->html('<span>Roles</span>', ['class' => 'menu-title']);
-        $menu->add(Link::toRoute('roles.index', '<i class="la la-key"></i> <span>'.__('Roles & Permissions'). '</span>')->addClass(route_is('roles.index') ? 'active' : ''));
+        $menu->add(Link::toRoute('roles.index', '<i class="la la-key"></i> <span>'.__('Roles & Permissions'). '</span>')->setActive(route_is('roles.*') ? 'active' : ''));
     }
 }
