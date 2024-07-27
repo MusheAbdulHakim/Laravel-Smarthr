3802fedae9a4a7716f2d9fe4c3b13bc00c70de20
musheabdulhakim <musheabdulhakim99@gmail.com>
2024-07-27 11:24:07 +0000

    @employee and @client added

--- app/Providers/MacrosServiceProvider.php
+++ app/Providers/MacrosServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\User;
 use Spatie\Menu\Laravel\Html;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class MacrosServiceProvider extends ServiceProvider
@@ -21,6 +22,22 @@ class MacrosServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Blade::directive('employee', function () {
+            return "<?php if(auth()->check() && auth()->user()->type === \App\Enums\UserType::EMPLOYEE): ?>";
+        });
+
+        Blade::directive('endemployee', function () {
+            return '<?php endif; ?>';
+        });
+
+        Blade::directive('client', function () {
+            return "<?php if(auth()->check() && auth()->user()->type === \App\Enums\UserType::CLIENT): ?>";
+        });
+
+        Blade::directive('endclient', function () {
+            return '<?php endif; ?>';
+        });
+        
         Html::macro('userAvatar', function ($name, $src, $link = 'javascript:void(0)', $alt = '') {
             return Html::raw(
                 '<h2 class="table-avatar">
