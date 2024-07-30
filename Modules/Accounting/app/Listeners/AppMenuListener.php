45cc26d4b9bdb9a6f275cf5fa59bbf0dea5335dc
musheabdulhakim <musheabdulhakim99@gmail.com>
2024-07-30 11:10:58 +0000

    auth and permission checks added to menu

--- Modules/Accounting/app/Listeners/AppMenuListener.php
+++ Modules/Accounting/app/Listeners/AppMenuListener.php
@@ -23,25 +23,27 @@ class AppMenuListener
      */
     public function handle(AppMenuEvent $event): void
     {
-        $menu = $event->menu;
-        $activeClass = route_is(["budget.categories.*","budgets.*","budget.expenses.*","budget.revenue.*"]) ? "active" : "";
-        $menu
-            ->submenu(
-                Html::raw('<a href="#" class="' . $activeClass . '"><i class="la la-files-o"></i><span> ' . __("Accounting") . '</span><span class="menu-arrow"></span></a>'),
-                Menu::new()
-                    ->add(
-                        Link::toRoute('budget.categories.index', __('Categories'))->addClass(route_is(['budget.categories.*']) ? 'active' : ''),
-                    )
-                    ->add(
-                        Link::toRoute('budgets.index', __('Budgets'))->addClass(route_is(['budgets.*']) ? 'active' : ''),
-                    )
-                    ->add(
-                        Link::toRoute('budget.expense.index', __('Budget Expenses'))->addClass(route_is(['budget.expense.*']) ? 'active' : ''),
-                    )
-                    ->add(
-                        Link::toRoute('budget.revenue.index', __('Budget Revenue'))->addClass(route_is(['budget.revenue.*']) ? 'active' : ''),
-                    )
-                    ->addParentClass('submenu')
-            );
+        if(auth()->user()->canAny(['view-budgetCategories','view-budgets','view-budgetExpenses','view-budgetRevenues'])){
+            $menu = $event->menu;
+            $activeClass = route_is(["budget.categories.*","budgets.*","budget.expenses.*","budget.revenue.*"]) ? "active" : "";
+            $menu
+                ->submenu(
+                    Html::raw('<a href="#" class="' . $activeClass . '"><i class="la la-files-o"></i><span> ' . __("Accounting") . '</span><span class="menu-arrow"></span></a>'),
+                    Menu::new()
+                        ->addIfCan('view-budgetCategories',
+                            Link::toRoute('budget.categories.index', __('Categories'))->addClass(route_is(['budget.categories.*']) ? 'active' : ''),
+                        )
+                        ->addIfCan('view-budgets',
+                            Link::toRoute('budgets.index', __('Budgets'))->addClass(route_is(['budgets.*']) ? 'active' : ''),
+                        )
+                        ->addIfCan('view-budgetExpenses',
+                            Link::toRoute('budget.expense.index', __('Budget Expenses'))->addClass(route_is(['budget.expense.*']) ? 'active' : ''),
+                        )
+                        ->addIfCan('view-budgetRevenues',
+                            Link::toRoute('budget.revenue.index', __('Budget Revenue'))->addClass(route_is(['budget.revenue.*']) ? 'active' : ''),
+                        )
+                        ->addParentClass('submenu')
+                );
+        }
     }
 }
