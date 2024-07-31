9e525ef514eccb63a102b236cc0e87639dccecf5
musheabdulhakim <musheabdulhakim99@gmail.com>
2024-07-31 02:35:16 +0000

    autoclockout users after every 8hrs

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Jobs\AutoClockoutUnsignedAttendances;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+
+Schedule::job(new AutoClockoutUnsignedAttendances())->cron("0 */8 * * *");
