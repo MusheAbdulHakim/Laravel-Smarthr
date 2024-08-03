462c862ba8ac351385864c1c6709fd9fe792adac
musheabdulhakim <musheabdulhakim99@gmail.com>
2024-08-03 14:06:47 +0000

    initial commit

---
+++ database/factories/EmployeeDeductionFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use App\Enums\UserType;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\EmployeeDeduction>
+ */
+class EmployeeDeductionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'employee_detail_id' => User::inRandomOrder()
+                ->where('type', UserType::EMPLOYEE)
+                ->whereHas('employeeDetail')
+                ->first()
+                ->employeeDetail()->id 
+            ?? User::factory()->create([
+                'type' => UserType::EMPLOYEE,
+            ])->id, 
+            'name' => $this->faker->word(),
+            'amount' => $this->faker->numberBetween(10,100),
+        ];
+    }
+}
