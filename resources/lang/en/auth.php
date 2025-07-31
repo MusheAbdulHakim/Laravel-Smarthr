10fdb8670d523c96b002a371f2ee61ed0def12d1
musheabdulhakim <musheabdulhakim99@gmail.com>
2025-07-31 15:45:39 -0700

    dependencies upgraded

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -4,6 +4,6 @@ declare(strict_types=1);
 
 return [
     'failed'   => 'These credentials do not match our records.',
-    'password' => 'The password is incorrect.',
+    'password' => 'The provided password is incorrect.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 ];
