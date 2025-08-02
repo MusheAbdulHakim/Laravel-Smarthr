75c5ed7426311b219876d5b15477fdb1577b6991
musheabdulhakim <musheabdulhakim99@gmail.com>
2025-08-01 18:15:19 -0700

    upgraded to laravel 12

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,8 +12,9 @@ use LaravelLang\Routes\Middlewares\LocalizationBySession;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__ . '/../routes/web.php',
-        commands: __DIR__ . '/../routes/console.php',
+        web: __DIR__.'/../routes/web.php',
+        commands: __DIR__.'/../routes/console.php',
+        channels: __DIR__.'/../routes/channels.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
--- routes/channels.php
+++ routes/channels.php
@@ -1,10 +1,7 @@
 <?php
 
-use App\Models\ChatMessage;
 use Illuminate\Support\Facades\Broadcast;
 
-
-// Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-//     return (int) $user->id === (int) $id;
-// });
-
+Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
+    return (int) $user->id === (int) $id;
+});
